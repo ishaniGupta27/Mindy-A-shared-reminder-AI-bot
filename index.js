89fdf38b22d147c70514a27e47751ab162f6de87
Ishani <ishanigupta27@github.com>
2018-04-12 02:10:36 -0700

    inital attempt to make it work on local for dev

---
+++ index.js
@@ -0,0 +1,28 @@
+var restify=require("restify");
+var builder=require("botbuilder");
+
+//we are creating a server using restify and it is listening !
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log('%s listening to %s',server.name,server.url)
+});
+
+server.get("/health_check_site",function(req,res,next){
+    res.send(200);
+    return next();
+});
+
+//For connection we need the id & password.
+var con = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword:process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new builder.UniversalBot(con);
+
+bot.dialog('/',function(session){
+
+    session.send("Hello World");
+});
+
+
