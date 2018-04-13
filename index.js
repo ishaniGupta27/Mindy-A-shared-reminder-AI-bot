c71260f8604e84fad985a499e8f0fb9d6a61cf3b
Ishani <ishanigupta27@github.com>
2018-04-12 17:32:41 -0700

    sending reminder

---
+++ index.js
@@ -0,0 +1,61 @@
+var restify=require("restify");
+var builder=require("botbuilder");
+var assert= require('assert');
+//var clients= require('restify-clients');
+//we are creating a server using restify and it is listening !
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log('%s listening to %s',server.name,server.url)
+});
+//This is where server is listening
+server.post('/health_check_site',function(req,res,next){
+    res.send(200);
+    return next();
+});
+
+//Your bot's logic is hosted as a web service that receives messages from users through the Connector service, and your bot's replies are sent to the Connector using HTTPS POST.
+//For connection we need the id & password.
+var con = new builder.ChatConnector({ //chatconnector connects us to outside world{
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword:process.env.MICROSOFT_APP_PASSWORD
+});
+
+
+server.post('/api/messages/new', con.listen()); 
+
+//The UniversalBot class forms the brains of your bot. It's responsible for managing all the conversations your bot has with a user. The ChatConnector class connects your bot to the Bot Framework Connector Service.
+var bot = new builder.UniversalBot(con);
+
+//dialogs are fundamental block to build conversation witht he user
+bot.dialog('/',[
+//You use the session.send method to send messages in response to a message from the user.
+    function (session){
+
+	    session.dialogData.obj={};
+	    builder.Prompts.text(session, "Hi, Who do you want to send the reminder ?");
+	    //session.beginDialog('Hi, Who do you want to send the reminder ?');
+	    //builder.Prompts.text(session,"Hi, Who do you want to send the reminder ?");
+    },
+    function (session, results){
+    	    session.dialogData.obj.receiver= results.response;
+	    builder.Prompts.text(session, "What is the reminder you want to send ?");
+	    //session.beginDialog('What is the reminder you want to send ?');
+    },
+    function (session, results) {
+        session.dialogData.obj.task = results.response;
+        session.send('Reminder Sent !');
+    },
+
+]);
+
+//Server 1 acting as a client 
+//var clients = require('restify-clients');
+//var assert= require('assert');
+/*var cl1 = clients.createJsonClient('http://127.0.0.1:3979');
+cl1.post('/health_check_site', function(err,req,res){
+       //assert.ifError(err);
+      console.log('%s %d','Connected',res.statusCode);
+ });
+ */
+
+
